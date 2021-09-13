4b734abab453a3e98f34c992393ec3836ce088d6
Lívia Almeida <leave.ah@gmail.com>
2021-09-13 16:39:39 -0300

    Remove loading from app

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
     <Header />
-		<div class="app-loading" v-if="loading">
-			<Coffee />
-		</div>
-    <router-view class="app-view" v-else />
+    <router-view
+			class="app-view"
+		/>
   </div>
 </template>
 
@@ -16,14 +15,6 @@ export default Vue.extend({
   components: {
     Header,
 	},
-	data() {
-		return {
-			loading: true,
-		}
-	},
-	mounted() {
-		setTimeout(() => { this.loading = false }, 2000)
-	},
 })
 </script>
 
@@ -32,43 +23,23 @@ export default Vue.extend({
 
 $loader-img: 250px;
 
+html {
+	scroll-behavior: smooth;
+}
+
 html, body, #app {
   width: 100%;
   height: 100%;
 }
 
 #app {
-  font-family: 'Bebas Neue', 'Open Sans', Helvetica;
-	color: $purple-main;
+  font-family: 'Open Sans', Helvetica;
+	color: $lv-purple;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 
   * {
     box-sizing: border-box;
   }
-}
-
-.app {
-	&-loading, &-view {
-		padding: 15*$m 3*$m 3*$m;
-		width: 100%;
-		display: flex;
-	}
-
-	&-loading {
-		height: 100%;
-		align-items: center;
-		justify-content: center;
-		background-color: rgba($purple-main, .5);
-	}
-
-	&-view {
-		flex-direction: column;
-		margin: 0 auto;
-		max-width: 100%;
-		overflow: hidden;
-		gap: 60px;
-		max-width: 650px;
-	}
 }
 </style>
