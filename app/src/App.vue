3e37d199fa01aa08f3917e3ea03e12227b9d12d3
Lívia Almeida <leave.ah@gmail.com>
2024-06-22 18:18:34 +0200

    Move app file to composition

--- app/src/App.vue
+++ app/src/App.vue
@@ -7,20 +7,16 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue'
+import mixpanel from 'mixpanel-browser'
+
 import Contact from '@/components/Contact.vue'
 import Experience from '@/components/Experience.vue'
 import Hero from '@/components/Hero.vue'
 import Knowledge from '@/components/Knowledge.vue'
 import Menu from '@/components/Menu.vue'
-</script>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import mixpanel from 'mixpanel-browser'
-
-export default defineComponent({
-  mounted() {
-    mixpanel.track('Page-view')
-  },
+onMounted(() => {
+  mixpanel.track('Page-view')
 })
 </script>
